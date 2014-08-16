de8a1f6426c92255b7fc0afb8501f88367f51e9b
bessdsv <bessdsv@mail.ru>
2014-08-16 11:06:28 +0400

    Update install script

--- install.js
+++ install.js
@@ -1,4 +1,5 @@
 require('child_process').exec(require('path').join('node_modules', '.bin', 'bower-installer') + ' --keep --remove');
 var fs = require('fs');
 fs.rmdirSync('scripts');
+fs.rmdirSync('node_modules');
 fs.unlinkSync('bower.json');
